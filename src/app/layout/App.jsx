9e9ab42bb107aa74d447cadbe7212d79bd9ea458
= <=>
2018-06-02 03:27:15 -0700

    Added Events-Mockup

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
-
+import { Container } from "semantic-ui-react";
+import EventDashBoard from "../../features/event/EventDashBoard/EventDashBoard";
+import NavBar from "../../features/nav/NavBar/NavBar";
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Re-vents!!!!</h1>
+        <NavBar />
+        <Container className="main">
+          <EventDashBoard />
+        </Container>
       </div>
     );
   }
